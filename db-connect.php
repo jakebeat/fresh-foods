a33d91afc2d7a786009b9f00d1cf212071e31a02
Jake <69029912+jakebeat@users.noreply.github.com>
2026-07-23 18:22:06 -0500

    Add database connection script

---
+++ db-connect.php
@@ -0,0 +1,14 @@
+<?php
+$config = require '/etc/freshfoods/db.php';
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4",
+        $config['user'],
+        $config['password'],
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    http_response_code(500);
+    die('Database connection failed.');
+}
